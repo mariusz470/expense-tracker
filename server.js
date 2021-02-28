cfa06fd90eee1eb4d9f5f92b6975a6b591b6ac26
Mariusz Kiciński <mariusz470@users.noreply.github.com>
2021-02-28 19:51:21 +0100

    Version 1.0.0

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ if (process.env.NODE_ENV === "development") {
 // app.get("/", (req, res) => res.send("Hello"));
 app.use("/api/v1/transactions", transactions);
 
+if (process.env.NODE_ENV === "production") {
+  // Set Static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
